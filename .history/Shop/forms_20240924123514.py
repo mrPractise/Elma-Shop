6cd991c977a19665f4db8f94ad292dff89d24333
mrPractise <coddingpractise@gmail.com>
2024-10-08 13:48:12 +0300

    Delete .history directory

--- .history/Shop/forms_20240924123514.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ShippingForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    address = forms.CharField(widget=forms.Textarea)
-    phone = forms.CharField(max_length=20)
--- .history/Shop/forms_20240924135959.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ShippingForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    address = forms.CharField(widget=forms.Textarea)
